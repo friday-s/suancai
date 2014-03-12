d04f1406e8d45437383ed564c0d741a881b8f6c1
wei.xue <wei.xue.wx@gmail.com>
2014-03-12 19:33:52 +0800

    add custom title bar

--- AndroidEventReplay/src/com/xue/aer/MainFrame.java
+++ AndroidEventReplay/src/com/xue/aer/MainFrame.java
@@ -22,10 +22,6 @@ public class MainFrame extends JFrame {
 
 	private JPanel contentPane;
 
-	private boolean isDragged = false;
-	private Point loc = null;
-	private Point tmp = null;
-
 	/**
 	 * Create the frame.
 	 */
@@ -38,7 +34,7 @@ public class MainFrame extends JFrame {
 		contentPane.setLayout(null);
 		// contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		TitleBar bar  = new TitleBar(Util.getImageIcon("android.png"),"afgjfjf");
+		TitleBar bar  = new TitleBar(this,Util.getImageIcon("android.png"),AER.APP_NAME,Util.getImageIcon("close.png"));
 		contentPane.add(bar);
 		//contentPane.add(initMenuBar(AER.MENU_BAR));
 		//
@@ -54,30 +50,7 @@ public class MainFrame extends JFrame {
 		 */
 		// contentPane.add(initTabledPane(AER.TABBED_PANE));
 
-		this.addMouseListener(new java.awt.event.MouseAdapter() {
-			public void mouseReleased(MouseEvent e) {
-				isDragged = false;
-				// 为指定的光标设置光标图像
-				setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
-			}
 
-			public void mousePressed(MouseEvent e) {
-				tmp = new Point(e.getX(), e.getY());
-				isDragged = true;
-				setCursor(new Cursor(Cursor.MOVE_CURSOR));
-			}
-		});
-
-		this.addMouseMotionListener(new MouseMotionAdapter() {
-			// 鼠标按键在组件上按下并拖动时调用。
-			public void mouseDragged(MouseEvent e) {
-				if (isDragged) {
-					loc = new Point(getLocation().x + e.getX() - tmp.x, getLocation().y + e.getY()
-							- tmp.y);
-					setLocation(loc);
-				}
-			}
-		});
 	}
 
 	private JTabbedPane initTabledPane(String[] tabs) {
--- AndroidEventReplay/src/com/xue/aer/TitleBar.java
+++ AndroidEventReplay/src/com/xue/aer/TitleBar.java
@@ -1,45 +1,122 @@
 package com.xue.aer;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Image;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SpringLayout;
+
+import com.xue.res.AER;
 
 public class TitleBar extends JPanel {
 
-	// private Image mTitleIcon;
-	private JLabel mTitleIcon;
-	private JLabel mTitleName;
+    private static final int TITLE_BAR_ICON_WIDTH = 20;
+    private static final int TITLE_BAR_ICON_HEIGHT = 20;
 
-	public TitleBar() {
-		initView();
-	}
+    private static final int TITLE_BAR_WIDTH = AER.WIDTH;
+    private static final int TITLE_BAR_HEIGHT = 25;
 
-	public TitleBar(Icon icon, String name) {
-		initView();
-		setTitleIcon(icon);
-		setTitleName(name);
-	}
+    private JFrame mContext;
+    // private Image mTitleIcon;
+    private JLabel mTitleIcon;
+    private JLabel mTitleName;
+    private JLabel mCloseIcon;
 
-	private void initView() {
-		mTitleIcon = new JLabel();
-		mTitleName = new JLabel();
-		setLayout(new BorderLayout());
-		setVisible(true);
-		setBounds(0, 0, 200, 80);
-		add(mTitleIcon, BorderLayout.WEST);
-		add(mTitleName,BorderLayout.EAST);
-	}
+    private boolean isDragged = false;
+    private Point loc = null;
+    private Point tmp = null;
 
-	public void setTitleName(String name) {
-		mTitleName.setText(name);
-	}
+    public TitleBar(JFrame context) {
+        this.mContext = context;
+        initView();
+    }
 
-	public void setTitleIcon(Icon icon) {
-		mTitleIcon.setIcon(icon);
+    public TitleBar(JFrame context, ImageIcon icon, String name) {
+        this.mContext = context;
+        initView();
+        setTitleIcon(icon);
+        setTitleName(name);
+    }
 
-	}
+    public TitleBar(JFrame context, ImageIcon icon, String name,ImageIcon closeIcon) {
+        this.mContext = context;
+        initView();
+        setTitleIcon(icon);
+        setTitleName(name);
+        setCloseIcon(closeIcon);
+    }
 
+    private void initView() {
+        mTitleIcon = new JLabel();
+        mTitleName = new JLabel();
+        mCloseIcon = new JLabel();
+        
+        SpringLayout springLayout = new SpringLayout();
+        setLayout(springLayout);
+        setVisible(true);
+        System.out.println(TITLE_BAR_WIDTH);
+        setBounds(0, 0, TITLE_BAR_WIDTH, TITLE_BAR_HEIGHT);
+        this.setBackground(Color.GRAY);
+
+        add(mTitleIcon);
+        add(mTitleName);
+        add(mCloseIcon);
+
+        springLayout.putConstraint(SpringLayout.WEST, mTitleIcon, 5,
+                SpringLayout.WEST, this);
+        springLayout.putConstraint(SpringLayout.WEST, mTitleName, 5,
+                SpringLayout.EAST, mTitleIcon);
+        springLayout.putConstraint(SpringLayout.EAST, mCloseIcon, 5,
+                SpringLayout.EAST, this);
+
+        this.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(MouseEvent e) {
+                isDragged = false;
+                // Ϊָ���Ĺ�����ù��ͼ��
+                mContext.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            }
+
+            public void mousePressed(MouseEvent e) {
+                tmp = new Point(e.getX(), e.getY());
+                isDragged = true;
+                mContext.setCursor(new Cursor(Cursor.MOVE_CURSOR));
+            }
+        });
+
+        this.addMouseMotionListener(new MouseMotionAdapter() {
+            // ��갴��������ϰ��²��϶�ʱ���á�
+            public void mouseDragged(MouseEvent e) {
+                if (isDragged) {
+                    loc = new Point(mContext.getLocation().x + e.getX() - tmp.x,
+                            mContext.getLocation().y + e.getY() - tmp.y);
+                    mContext.setLocation(loc);
+                }
+            }
+        });
+    }
+    
+    public void setCloseIcon(ImageIcon icon){
+        icon.setImage(icon.getImage().getScaledInstance(TITLE_BAR_ICON_WIDTH,
+                TITLE_BAR_ICON_HEIGHT, Image.SCALE_DEFAULT));
+        mCloseIcon.setIcon(icon);
+    }
+
+    public void setTitleName(String name) {
+        mTitleName.setText(name);
+    }
+
+    public void setTitleIcon(ImageIcon icon) {
+        icon.setImage(icon.getImage().getScaledInstance(TITLE_BAR_ICON_WIDTH,
+                TITLE_BAR_ICON_HEIGHT, Image.SCALE_DEFAULT));
+        mTitleIcon.setIcon(icon);
+    }
 }
